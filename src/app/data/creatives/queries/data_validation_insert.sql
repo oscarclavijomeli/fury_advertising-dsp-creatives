143ce4934c6aa69edd0918763029defec79446d4
Cristian Camilo Jimenez Mancera <cristian.jimenezmancera@mercadolibre.com.co>
2023-02-03 08:43:59 -0500

    fix_data_quality_query (#42)

--- src/app/data/creatives/queries/data_validation_insert.sql
+++ src/app/data/creatives/queries/data_validation_insert.sql
@@ -1,2 +1,3 @@
 INSERT INTO meli-bi-data.SBOX_DSPCREATIVOS.data_quality_validations
-VALUES ('{execution_date}', '{process}', {success}, {success_statistics}, {expectations})
+(execution_date, process, success, success_statistics, expectations, site)
+VALUES ('{execution_date}', '{process}', {success}, {success_statistics}, {expectations}, {site})
